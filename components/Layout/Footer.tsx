903ba7e48fdd3c04cd3b8e372d0e037eed23f7be
dodo <110053481+MdodoIng@users.noreply.github.com>
2025-06-15 12:30:17 +0530

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const navItems = [
   { id: "home", icon: "mdi:home", label: "Home" },
@@ -10,9 +10,43 @@ const navItems = [
 
 const Footer = () => {
   const [active, setActive] = useState("home");
+  const [visible, setVisible] = useState(true);
+  const lastScrollY = useRef(0);
+  const timeoutRef = useRef<any>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY.current) {
+        setVisible(false);
+      } else {
+        setVisible(true);
+      }
+
+      lastScrollY.current = currentScrollY;
+
+     
+      clearTimeout(timeoutRef.current!);
+      timeoutRef.current = setTimeout(() => setVisible(true), 200);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearTimeout(timeoutRef.current!);
+    };
+  }, []);
 
   return (
-    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 bg-black px-3 py-2 rounded-full flex space-x-3 shadow-2xl z-50 lg:hidden">
+    <div
+      className={`fixed bottom-6 left-1/2 -translate-x-1/2 bg-black px-3 py-2 rounded-full flex space-x-3 shadow-2xl z-50 transition-all duration-300 lg:hidden ${
+        visible
+          ? "opacity-100 translate-y-0"
+          : "opacity-0 translate-y-5 pointer-events-none"
+      }`}
+    >
       {navItems.map((item) => (
         <button
           key={item.id}
