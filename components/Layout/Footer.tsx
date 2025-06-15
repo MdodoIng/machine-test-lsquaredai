e721578faa082b9e993106d905190087dc8fcb53
dodo <110053481+MdodoIng@users.noreply.github.com>
2025-06-15 12:31:09 +0530

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
 
   return (
     <div
-      className={`fixed bottom-6 left-1/2 -translate-x-1/2 bg-black px-3 py-2 rounded-full flex space-x-3 shadow-2xl z-50 transition-all duration-300 lg:hidden ${
+      className={`fixed bottom-6 left-1/2 -translate-x-1/2 bg-black px-3 py-2 rounded-full flex space-x-3 shadow-2xl z-50 transition-all duration-300 md:hidden ${
         visible
           ? "opacity-100 translate-y-0"
           : "opacity-0 translate-y-5 pointer-events-none"
