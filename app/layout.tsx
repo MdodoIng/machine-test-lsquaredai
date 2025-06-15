1100b398c907487a9e151e271e6cc581770ea3a8
dodo <110053481+MdodoIng@users.noreply.github.com>
2025-06-15 14:08:41 +0530

    modified:   app/layout.tsx modified:   bun.lockb modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 
 const poppins = Poppins({
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={`${poppins.className} antialiased flex w-screen lg:h-screen lg:overflow-hidden max-w-lg:overflow-x-hidden`}>
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
